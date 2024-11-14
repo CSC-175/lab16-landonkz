89aa5a940bfd167e8bebcc8bef500b37f08600dd
Landon Kurtz <landonkz@umich.edu>
2024-11-14 12:10:28 -0500

    1st attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,33 @@
 // Code to implement the gcf function goes here
+
+int r;
+
+int gcf(int a, int b) {
+    if (a > b) {
+        r = a % b;
+
+        if (r != 0) {
+            a = b;
+            b = r;
+            gcf(a, b);
+        }
+        else {
+            return b;
+        }
+    }
+    else if (a < b) {
+        r = b % a;
+
+        if (r != 0) {
+            b = a;
+            a = r;
+            gcf(b, a);
+        }
+        else {
+            return b;
+        }
+    }
+    else {
+        return a;
+    }
+}
